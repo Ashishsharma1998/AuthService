0a35e2b8f1ae7e466c923e3cf5811eb337628fd1
Ashish sharma <sharma07101998@gmail.com>
2023-01-09 15:36:46 +0530

    added roles and user_roles through tables

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const apiRoutes = require("./routes/index");
 // const userRepositroy = require("./repository/user-repository");
 
 // const userService = require("./services/user-service");
+const sequelize = require("sequelize");
+// const db = require("./models/index");
+
+const { User, Role } = require("./models/index");
 const app = express();
 
 const serverUpAndStart = () => {
@@ -16,6 +20,10 @@ const serverUpAndStart = () => {
 
   app.listen(PORT, async () => {
     console.log(`server is started at port ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+
     // const repo = new userRepositroy();
     // const response = await repo.getById(1);
     // console.log(response);
@@ -23,6 +31,14 @@ const serverUpAndStart = () => {
     // const obj = new userService();
     // const token = obj.createToken({ email: "ashish@gmail.com", id: "1" });
     // console.log(token);
+
+    // const user = await User.findByPk(1);
+    const role = await Role.findByPk(1);
+    // user.addRole(role);
+
+    // const response = await user.getRoles();
+    const response = await role.getUsers();
+    console.log(response);
   });
 };
 
